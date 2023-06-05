522b765e36f1f9fe3af90ea6ff608b717f7e3a59
Luka Zibert <luka.zibert21@gmail.com>
2023-06-05 21:06:59 +0200

    added score UI and updating script

--- Assets/Scripts/GoalLineController.cs
+++ Assets/Scripts/GoalLineController.cs
@@ -9,19 +9,40 @@ public class GoalLineController : MonoBehaviour
     private int player1Score;
     private int player2Score;
 
+    GameObject textPlayer1;
+    GameObject textPlayer2;
+
+    UpdateScore updateTextPlayer1;
+    UpdateScore updateTextPlayer2;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Find the game object with the UpdateText script attached
+        GameObject textPlayer1 = GameObject.Find("Player1-Score");
+        GameObject textPlayer2 = GameObject.Find("Player2-Score");
+
+        // Get the UpdateText script component from the found game object
+        updateTextPlayer1 = textPlayer1.GetComponent<UpdateScore>();
+        updateTextPlayer2 = textPlayer2.GetComponent<UpdateScore>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("soccer_ball"))
+        if (other.gameObject.name == "Icosphere")
         {
             if (isFirstPlayerTurn)
             {
                 player1Score++;
                 UnityEngine.Debug.Log("Player 1 Scored! Score: " + player1Score);
+                updateTextPlayer1.UpdatePlayerScore(player1Score);
             }
             else
             {
                 player2Score++;
                 UnityEngine.Debug.Log("Player 2 Scored! Score: " + player2Score);
+                updateTextPlayer2.UpdatePlayerScore(player2Score);
             }
 
             if (player1Score >= 5 && player2Score >= 5)
---
+++ Assets/Scripts/UpdateScore.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateScore : MonoBehaviour
+{
+
+    private Text textObject; // Corrected variable type
+
+    void Start()
+    {
+        // Get the Text component from the game object
+        textObject = GetComponent<Text>();
+
+        UnityEngine.Debug.Log("Initial textObject " + textObject.text);
+
+        // Update the text value
+        textObject.text = "0";
+
+        UnityEngine.Debug.Log("Updated textObject " + textObject.text);
+    }
+
+    public void UpdatePlayerScore(int score){
+        textObject.text = score.ToString();
+        // UnityEngine.Debug.Log("Player Scored! Score: " + score);
+    }
+
+    
+}
